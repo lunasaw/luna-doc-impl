434895b19d74e7c2b32626d38417510c3e54d82c
chenzhangyue <chenzhangyue01@weidian.com>
2023-05-02 19:17:27 +0800

    :sparkles: gracefully-log

--- gracefully-log-test/src/main/java/io/github/lunasaw/test/LocalLogServiceImpl.java
+++ gracefully-log-test/src/main/java/io/github/lunasaw/test/LocalLogServiceImpl.java
@@ -1,25 +1,24 @@
-package io.github.lunasaw.log.service.impl;
+package io.github.lunasaw.test;
 
-import io.github.lunasaw.log.entity.OperatorLog;
-import io.github.lunasaw.log.service.LogService;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import io.github.lunasaw.log.entity.OperatorLog;
+import io.github.lunasaw.log.service.LogService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author luna
  * @date 2023/5/2
  */
 @Service
-@Slf4j(topic = "OPERATE")
-public class DefaultLogServiceImpl implements LogService {
+public class LocalLogServiceImpl implements LogService {
 
-    public static final Logger operate_log = LoggerFactory.getLogger("OPERATE");
-
+    public static final Logger log = LoggerFactory.getLogger("OPERATE");
 
     @Override
     public void printLog(OperatorLog operatorLog) {
@@ -33,16 +32,16 @@ public class DefaultLogServiceImpl implements LogService {
         LogLevel logLevel = Optional.ofNullable(operatorLog.getLevel()).orElse(LogLevel.INFO);
         if (LogLevel.INFO == logLevel) {
             log.info("bizNo:{},operator:{},category:{},success:{}",
-                    operatorLog.getBizNo(), operatorLog.getOperator(),
-                    operatorLog.getCategory(), operatorLog.getSuccess());
+                operatorLog.getBizNo(), operatorLog.getOperator(),
+                operatorLog.getCategory(), operatorLog.getSuccess());
         } else if (LogLevel.WARN == logLevel) {
             log.warn("bizNo:{},operator:{}, category:{},success:{}",
-                    operatorLog.getBizNo(), operatorLog.getOperator(),
-                    operatorLog.getCategory(), operatorLog.getSuccess());
+                operatorLog.getBizNo(), operatorLog.getOperator(),
+                operatorLog.getCategory(), operatorLog.getSuccess());
         } else if (LogLevel.ERROR == logLevel) {
             log.error("bizNo:{},operator:{},category:{},fail:{}",
-                    operatorLog.getOperator(), operatorLog.getBizNo(),
-                    operatorLog.getCategory(), operatorLog.getFail());
+                operatorLog.getOperator(), operatorLog.getBizNo(),
+                operatorLog.getCategory(), operatorLog.getFail());
         }
     }
 }
--- gracefully-log/src/main/java/io/github/lunasaw/log/aspect/LogRecordAspect.java
+++ gracefully-log/src/main/java/io/github/lunasaw/log/aspect/LogRecordAspect.java
@@ -1,19 +1,19 @@
 package io.github.lunasaw.log.aspect;
 
-import io.github.lunasaw.log.anno.LogRecord;
-import io.github.lunasaw.log.entity.OperatorLog;
-import io.github.lunasaw.log.service.LogService;
-import io.github.lunasaw.log.service.OperatorGetService;
-import lombok.SneakyThrows;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -22,26 +22,27 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-
+import io.github.lunasaw.log.anno.LogRecord;
+import io.github.lunasaw.log.entity.OperatorLog;
+import io.github.lunasaw.log.service.LogService;
+import io.github.lunasaw.log.service.OperatorGetService;
+import lombok.SneakyThrows;
 
 /**
  * @author luna
  * @date 2023/5/1
  */
 @Aspect
+@Component
+@ConditionalOnClass({LogService.class, OperatorGetService.class})
 public class LogRecordAspect implements ApplicationContextAware {
 
     private static final SpelExpressionParser PARSER = new SpelExpressionParser();
     @Resource
-    private OperatorGetService operatorGetService;
+    private OperatorGetService                operatorGetService;
     @Resource
-    private LogService logService;
-    private ApplicationContext applicationContext;
-
+    private LogService                        logService;
+    private ApplicationContext                applicationContext;
 
     @Around("@annotation(io.github.lunasaw.log.anno.LogRecord)")
     public Object invoke(ProceedingJoinPoint joinPoint) {
@@ -55,12 +56,11 @@ public class LogRecordAspect implements ApplicationContextAware {
      */
     @SneakyThrows
     private Object execute(ProceedingJoinPoint joinPoint) {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        MethodSignature signature = (MethodSignature)joinPoint.getSignature();
         Method method = signature.getMethod();
         LogRecord annotation = method.getAnnotation(LogRecord.class);
         ExpressionEvaluatorContext evaluationContext = new ExpressionEvaluatorContext(joinPoint.getTarget(), method, joinPoint.getArgs());
         evaluationContext.setBeanResolver(new BeanFactoryResolver(applicationContext));
-
 
         // 只打印用户操作日志
         String operator = parse(annotation.operator(), evaluationContext, String.class);
@@ -68,7 +68,6 @@ public class LogRecordAspect implements ApplicationContextAware {
         String bizNo = parse(annotation.bizNo(), evaluationContext, String.class);
         String category = parse(annotation.category(), evaluationContext, String.class);
         Boolean condition = parse(annotation.condition(), evaluationContext, Boolean.class);
-
 
         if (StringUtils.isEmpty(operator)) {
             operator = operatorGetService.getUser();
@@ -82,12 +81,12 @@ public class LogRecordAspect implements ApplicationContextAware {
         } catch (Throwable e) {
             String message = e.getMessage();
             paramMap.put("_error", message);
-            OperatorLog operatorLog = OperatorLog.builder().level(LogLevel.ERROR).bizNo(bizNo).operator(operator).fail(fail).category(category).condition(condition).build();
+            OperatorLog operatorLog = OperatorLog.builder().level(LogLevel.ERROR).bizNo(bizNo).operator(operator).fail(fail).category(category)
+                .condition(condition).build();
             logService.printLog(operatorLog);
             throw e;
         }
         ExpressionEvaluatorContext.putVariable(paramMap);
-
 
         // 获取父线程的数据
         Map<String, Object> variables = ExpressionEvaluatorContext.getVariables();
@@ -97,14 +96,14 @@ public class LogRecordAspect implements ApplicationContextAware {
             }
         }
         String success = parse(annotation.success(), evaluationContext, String.class);
-        OperatorLog operatorLog = OperatorLog.builder().level(LogLevel.INFO).bizNo(bizNo).operator(operator).success(success).category(category).condition(condition).build();
+        OperatorLog operatorLog = OperatorLog.builder().level(LogLevel.INFO).bizNo(bizNo).operator(operator).success(success).category(category)
+            .condition(condition).build();
 
         logService.printLog(operatorLog);
 
         ExpressionEvaluatorContext.popVariable();
         return ret;
     }
-
 
     public <T> T parse(String content, StandardEvaluationContext context, Class<T> clazz) {
         return PARSER.parseExpression(resolve(content)).getValue(context, clazz);
@@ -114,12 +113,12 @@ public class LogRecordAspect implements ApplicationContextAware {
      * 作用是读取yml里面的值
      *
      * @param value 例如：1. #{${ttt.xxx}}会读取yml的ttt.xxx: read配置值，替换为#{read}
-     *              2.#{read}直接返回#{read}
+     * 2.#{read}直接返回#{read}
      * @return #{read}
      */
     private String resolve(String value) {
         if (this.applicationContext != null && this.applicationContext instanceof ConfigurableBeanFactory) {
-            return ((ConfigurableBeanFactory) this.applicationContext).resolveEmbeddedValue(value);
+            return ((ConfigurableBeanFactory)this.applicationContext).resolveEmbeddedValue(value);
         }
         return value;
     }
